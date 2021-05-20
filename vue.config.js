f621b429162733f817d7c2594bb521918bcb205f
code1379 <coderxialuo@gmail.com>
2021-05-20 18:55:33 +0800

    使用normalize.css初始样式 base.css规定html的字体大小 vue.config.js配置路径别名

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components",
+        service: "@/service",
+        views: "@/views"
+      }
+    }
+  }
+};
