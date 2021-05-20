5beb482510d3b08d9114623fb78c13e920f1b2b0
code1379 <coderxialuo@gmail.com>
2021-05-20 19:03:31 +0800

    关闭eslint的一些报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    semi: 0,
-    quotes: 0
+    semi: "off",
+    quotes: "off"
   }
 };
